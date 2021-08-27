14af5c33fa47f981aba066132072791f3b7e217a
BNB Punks <bnbpunksdev@gmail.com>
2021-08-27 15:48:50 -0700

    inital upload

--- src/contracts/BNBPunks.sol
+++ src/contracts/BNBPunks.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
