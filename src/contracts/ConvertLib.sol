3993ba6e2fab265c8f78c63f71df703eeb5476d5
BNB Punks <bnbpunksdev@gmail.com>
2021-08-27 16:58:25 -0700

    inital upload

--- src/contracts/ConvertLib.sol
+++ src/contracts/ConvertLib.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.0;
 
 library ConvertLib{
-	function convert(uint amount,uint conversionRate) returns (uint convertedAmount)
+	public function convert(uint amount,uint conversionRate) returns (uint convertedAmount)
 	 {
 		return amount * conversionRate;
 	}
