989b4cebbad53bfff3291c9de182298c83b7860d
BNB Punks <bnbpunksdev@gmail.com>
2021-08-27 20:22:48 -0700

    inital upload

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
 var BNBPunks = artifacts.require("./BNBPunks.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, BNBPunks);
   deployer.deploy(BNBPunks);
 };
